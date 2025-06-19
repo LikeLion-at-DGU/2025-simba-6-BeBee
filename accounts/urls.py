9a86962ba691c3c7556e6ecc758fd4967429d4d1
yunheewon <yunheewon66@gmail.com>
2025-06-19 18:04:37 +0900

    feat: 회원가입 로그인 로그아웃(#20)

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from .views import *
 app_name = "accounts"
 urlpatterns = [
     path('login/', login, name='login'),
+    path('lpgout/', logout, name="logout"),
     path('signup/', signup, name='signup'),
 ]
+
