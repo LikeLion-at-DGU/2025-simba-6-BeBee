a8af3714031d4bdf495d0d97953ee4decec54297
yunheewon <yunheewon66@gmail.com>
2025-06-20 20:53:26 +0900

    feature: 팔로우팔로잉 기능 및 수정(#24)

--- accounts/models.py
+++ accounts/models.py
@@ -11,10 +11,7 @@ class Profile(models.Model):
 
     profile_image = models.ImageField(upload_to='profile_images/',blank=True,null=True,default='default_images/default.png')
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     honey_count=models.IntegerField(default=0)
 
     completed_todo_count=models.IntegerField(default=0)
@@ -22,20 +19,10 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-=======
->>>>>>> 60b1e50 (feat: #24-팔로우구현)
-=======
->>>>>>> 60b1e50 (feat: #24-팔로우구현)
-=======
->>>>>>> 60b1e50 (feat: #24-팔로우구현)
-=======
->>>>>>> 60b1e50 (feat: #24-팔로우구현)
+
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
         
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
