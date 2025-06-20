8130d1b17ce3cae103e3642098de87912849cdb0
seoyeon435 <0305psy@naver.com>
2025-06-20 22:23:55 +0900

    WIP(#48):중간업로드

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import auth
 from django.contrib.auth.models import User
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 # 로그인
 def login(request):
