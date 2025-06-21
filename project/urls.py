42d6f235c07ba410baea2d3d0984c0b060886b98
yunheewon <yunheewon66@gmail.com>
2025-06-21 15:14:43 +0900

    충돌 해결(#60)

--- project/urls.py
+++ project/urls.py
@@ -24,10 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')), # default 로 mainpage 연결!,
     path('accounts/', include('accounts.urls')),
-    path('planner/', include('planner.urls')),
-<<<<<<< HEAD
+
     path('users/', include('users.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-]
->>>>>>> upstream
+
