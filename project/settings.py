ebdc6183c786887105a31dd303319d074895405b
HyoDong Jo <whgyehdjhd@naver.com>
2025-06-23 21:48:37 +0900

    fix:프로필사진 수정

--- project/settings.py
+++ project/settings.py
@@ -129,5 +129,5 @@ STATICFILES_DIRS = [BASE_DIR / 'project' / 'static'] # 어디서 static 가져�
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_ROOT=os.path.join(BASE_DIR,'project','media')
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
